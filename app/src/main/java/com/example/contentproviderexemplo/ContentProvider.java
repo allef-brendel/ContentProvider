58f98008dc436f13f8c99130a15c77896cd5f0a0
Allef Brendel <allef.brendel@dce.ufpb.br>
2019-08-23 09:22:58 -0300

    provider

--- app/src/main/java/com/example/contentproviderexemplo/ContentProvider.java
+++ app/src/main/java/com/example/contentproviderexemplo/ContentProvider.java
@@ -1,41 +1,63 @@
 package com.example.contentproviderexemplo;
 
+import android.content.ContentUris;
 import android.content.ContentValues;
+import android.content.Context;
 import android.content.UriMatcher;
 import android.database.Cursor;
-import android.database.MatrixCursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.database.sqlite.SQLiteQueryBuilder;
 import android.net.Uri;
+
+import java.util.HashMap;
 
 public class ContentProvider extends android.content.ContentProvider {
 
-    private static String[] colunas = {"nome"};
-
     public static final String AUTHORITY = "com.example.contentproviderexemplo";
-    public static final Uri CONTENT_URI = Uri.parse("content://" + AUTHORITY + "/nome");
+    public static final Uri CONTENT_URI = Uri.parse("content://" + AUTHORITY + "/pessoas");
+
+    static final String NAME = "nome";
+    static final String GRADE = "grade";
+    static final String _ID = "_id";
 
     public static final int PESSOA = 1;
     public static final int PESSOA_ID = 2;
 
-    private static final UriMatcher uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
-    static {
-        uriMatcher.addURI(AUTHORITY, "pessoa", PESSOA);
-        uriMatcher.addURI(AUTHORITY, "pessoa_id", PESSOA_ID);
-    }
+    private static HashMap<String, String> PESSOAS_MAP;
 
+    static final UriMatcher uriMatcher;
+    static {
+        uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+        uriMatcher.addURI(AUTHORITY, "pessoa", PESSOA);
+        uriMatcher.addURI(AUTHORITY, "pessoa_id/#", PESSOA_ID);
+    }
 
     @Override
     public boolean onCreate() {
+        Context context = getContext();
+        DatabaseHelper dbHelper = new DatabaseHelper(context);
+        db = dbHelper.getWritableDatabase();
+
         return true;
     }
 
     @Override
     public Cursor query(Uri uri, String[] strings, String s, String[] strings1, String s1) {
+        SQLiteQueryBuilder qb = new SQLiteQueryBuilder();
+        qb.setTables(PESSOAS_TABLE_NAME);
 
-
-        MatrixCursor cursor = new MatrixCursor(colunas);
-        for (String nome : new String[]{"José", "Maria", "Pedro"}) {
-            cursor.addRow(new Object[]{0, nome});
-        }
+       /* switch (uriMatcher.match(uri)){
+            case PESSOA:
+                qb.setProjectionMap(PESSOAS_MAP);
+                break;
+            case PESSOA_ID:
+                qb.appendWhere( _ID + "=" + uri.getPathSegments().get(1));
+                break;
+        }*/
+        Cursor cursor = qb.query(db, strings, s, strings1, null, null, s1);
+        cursor.setNotificationUri(getContext().getContentResolver(), uri);
         return cursor;
     }
 
@@ -46,7 +68,14 @@ public class ContentProvider extends android.content.ContentProvider {
 
     @Override
     public Uri insert(Uri uri, ContentValues contentValues) {
-        return null;
+        long rowID = db.insert(	PESSOAS_TABLE_NAME, "", contentValues);
+
+        if (rowID > 0) {
+            Uri _uri = ContentUris.withAppendedId(CONTENT_URI, rowID);
+            getContext().getContentResolver().notifyChange(_uri, null);
+            return _uri;
+        }
+        throw new SQLException("Failed to add a record into " + uri);
     }
 
     @Override
@@ -58,4 +87,33 @@ public class ContentProvider extends android.content.ContentProvider {
     public int update(Uri uri, ContentValues contentValues, String s, String[] strings) {
         return 0;
     }
+
+    private SQLiteDatabase db;
+    static final String DATABASE_NAME = "bancoDeDados";
+    static final String PESSOAS_TABLE_NAME = "pessoas";
+    static final int DATABASE_VERSION = 1;
+    static final String CREATE_DB_TABLE =
+            " CREATE TABLE " + PESSOAS_TABLE_NAME +
+                    " (_id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                    " nome TEXT NOT NULL, " +
+                    " grade TEXT NOT NULL);";
+
+
+    private static class DatabaseHelper extends SQLiteOpenHelper {
+        DatabaseHelper(Context context){
+            super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        }
+
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+            db.execSQL(CREATE_DB_TABLE);
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            db.execSQL("DROP TABLE IF EXISTS " +  PESSOAS_TABLE_NAME);
+            onCreate(db);
+        }
+    }
+
 }
--- app/src/main/java/com/example/contentproviderexemplo/MainActivity.java
+++ app/src/main/java/com/example/contentproviderexemplo/MainActivity.java
@@ -2,13 +2,63 @@ package com.example.contentproviderexemplo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button botaoAdd;
+    private Button botaoVer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        botaoAdd = findViewById(R.id.botaoadd);
+        botaoVer = findViewById(R.id.botaover);
+
+        botaoAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ContentValues values = new ContentValues();
+                values.put(ContentProvider.NAME, "ALLEF");
+
+                values.put(ContentProvider.GRADE, "helooo");
+
+                Uri uri = getContentResolver().insert(
+                        ContentProvider.CONTENT_URI, values);
+
+                Toast.makeText(getBaseContext(),
+                        uri.toString(), Toast.LENGTH_LONG).show();
+            }
+        });
+
+        botaoVer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String URL = "com.example.contentproviderexemplo";
+
+                Uri pessoas = Uri.parse(URL);
+                Cursor c = getContentResolver().query(pessoas, null, null, null, "nome");
+
+                if (c.moveToFirst()) {
+                    do {
+                        Toast.makeText(MainActivity.this, "TAG> "+ c.getString(c.getColumnIndex(ContentProvider.NAME)), Toast.LENGTH_SHORT).show();
+                    } while (c.moveToNext());
+                }
+            }
+        });
+
+
+
     }
 }
